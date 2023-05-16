9019e3c2f5ed3a6c45accc2a75235536f6a59887
rhmnx <116880430+rhmnx@users.noreply.github.com>
2023-05-16 10:19:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,157 +1,487 @@
 import pygame
+import os
+import random
+import csv
 
 pygame.init()
- 
-#Global Variable
+
+
 WIDTH = 800
-HEIGHT = 600
+HIEGHT = int(WIDTH * 0.8)
+
+screen = pygame.display.set_mode((WIDTH, HIEGHT))
+pygame.display.set_caption('Cyber Shooter')
+
 #set framerate
-FPS = 60
-#FPS
 clock = pygame.time.Clock()
-#define status game
-game_status = True 
+FPS = 60
+
+#define game variables
+FALL = 0.75
+ROWS = 16
+COLUMN = 150
+TILE_SIZE = HIEGHT // ROWS
+TILE_TYPES = 28
+level = 1
+
 #define player action variables
 moving_left = False
 moving_right = False
-falling = 0.8
-#screen
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Cyber Shooter')
+shoot = False
+
+
+#load images
+#store tiles in a list
+img_list = []
+for x in range(TILE_TYPES):
+	img = pygame.image.load(f'img/Tile/{x}.png')
+	img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+	img_list.append(img)
+#bullet
+bullet_img = pygame.image.load('img/icons/bullet.png').convert_alpha()
+#pick up boxes
+health_box_img = pygame.image.load('img/icons/health.png').convert_alpha()
+health_box_img = pygame.transform.rotozoom(health_box_img, 0, 0.06).convert_alpha()
+ammo_box_img = pygame.image.load('img/icons/ammo_box.png').convert_alpha()
+item_boxes = {
+	'Health'	: health_box_img,
+	'Ammo'		: ammo_box_img,
+}
+
+
+#define colours
+BG = (144, 201, 120)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+
+#define font
+font = pygame.font.SysFont('Futura', 30)
+
+def draw_text(text, font, text_col, x, y):
+	img = font.render(text, True, text_col)
+	screen.blit(img, (x, y))
+
+
+def draw_bg():
+	screen.fill(BG)
+
 
 class Hero(pygame.sprite.Sprite):
-    def __init__(self, x, y, speed):
-        pygame.sprite.Sprite.__init__(self)
-        
-        self.x = x
-        self.y = y
-        self.scale = 3
-        self.speed = speed
-        self.direction = 1
-        self.flip = False
-        self.jump = False
-        self.ground = 300
-        self.gravity = 0
-        self.animation_list = []
-        self.action = 0
-        self.frame_index = 0
-        self.update_time = pygame.time.get_ticks()
-        
-        temp_list = []
-        for i in range(1,5):
-            img = pygame.image.load(f'Hero/cyborg/idle{i}.png').convert_alpha()
-            img = pygame.transform.scale(img, (int(img.get_width() * self.scale), int(img.get_height() * self.scale)))
-            temp_list.append(img)
-        self.animation_list.append(temp_list)
+	def __init__(self, char_type, x, y, speed, ammo):
+		pygame.sprite.Sprite.__init__(self)
 
-        temp_list = [] 
-        for i in range(1,7):
-            img = pygame.image.load(f'Hero/cyborg/run{i}.png').convert_alpha()
-            img = pygame.transform.scale(img, (int(img.get_width() * self.scale), int(img.get_height() * self.scale)))
-            temp_list.append(img)
-        self.animation_list.append(temp_list)
+		self.x = x
+		self.y = y
+		self.scale = 2.5
+		self.alive = True
+		self.char_type = char_type
+		self.speed = speed
+		self.ammo = ammo
+		self.start_ammo = ammo
+		self.shoot_cooldown = 0
+		self.health = 100
+		self.max_health = self.health
+		self.direction = 1
+		self.gravity = 0
+		self.jump = False
+		self.in_air = True
+		self.flip = False
+		self.animation_list = []
+		self.frame_index = 0
+		self.action = 0
+		self.update_time = pygame.time.get_ticks()
+		#ai specific variables
+		self.move_counter = 0
+		self.vision = pygame.Rect(0, 0, 150, 20)
+		self.idling = False
+		self.idling_counter = 0
+		
+		#load all images for the players
+		animation_types = ['Idle', 'Run', 'Jump', 'Death']
+		for animation in animation_types:
+			#reset temporary list of images
+			temp_list = []
+			#count number of files in the folder
+			num_of_frames = len(os.listdir(f'img/{self.char_type}/{animation}'))
+			for i in range(num_of_frames):
+				img = pygame.image.load(f'img/{self.char_type}/{animation}/{i}.png').convert_alpha()
+				img = pygame.transform.scale(img, (int(img.get_width() * self.scale), int(img.get_height() * self.scale)))
+				temp_list.append(img)
+			self.animation_list.append(temp_list)
 
-        self.image = self.animation_list[self.action][int(self.frame_index)]
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
-
-    def move(self, moving_left, moving_right):
-        #reset movement var
-        dx = 0
-        dy = 0
-
-        #assign movement var if moving left or right
-        if moving_left:
-            dx = -self.speed
-            self.flip = True
-            self.direction = -1
-        if moving_right:
-            dx = self.speed
-            self.flip = False
-            self.direction = -1
-            
-        #jump 
-        if self.jump == True and self.ground == 300:
-            self.gravity = -13
-            self.jump = False
-        
-        #update gravity
-        self.gravity += falling
-        if self.gravity > 10:
-            self.gravity
-        dy += self.gravity
-
-        #update rect
-        self.rect.x += dx
-        self.rect.y += dy
-
-        if self.rect.bottom + dy > self.ground:
-            self.rect.bottom = self.ground 
-          
-    def animation(self):
-        #update animation
-        animation_cooldown = 100
-        #update image depending on current frame
-        self.image = self.animation_list[self.action][self.frame_index]
-        #check if enough time
-        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
-            self.update_time = pygame.time.get_ticks()
-            self.frame_index +=1
-        #if the animation has run out the reset back to the start
-        if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
-        
-    def update_action(self, new_action):
-        #check if the new action is different to the previous one
-        if new_action != self.action:
-            self.action = new_action
-            #reset 
-            self.frame_index = 0
-            self.update_time = pygame.time.get_ticks()
-
-    def draw(self):
-        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
-    
-
-#player
-player = Hero( 200, 300, 5)
-
-#while True:
-while game_status:
-    for event in pygame.event.get():
-        #quit game
-        if event.type == pygame.QUIT:
-            game_status = False
-            
-        #keyboard press
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                moving_left = True
-            if event.key == pygame.K_d:
-                moving_right = True
-            if event.key == pygame.K_w:
-                player.jump = True
-
-        #keyboard button released
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                moving_left = False
-            if event.key == pygame.K_d:
-                moving_right = False
-
-    #screen blit
-    screen.fill('white')
-    player.draw()
-    player.move(moving_left, moving_right)
-    player.animation()
-
-    #update player action
-    if moving_left or moving_right:
-        player.update_action(1)
-    else:
-        player.update_action(0)
-
-    pygame.display.update()
-    clock.tick(60)
+		self.image = self.animation_list[self.action][self.frame_index]
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
 
 
+	def update(self):
+		self.update_animation()
+		self.check_alive()
+		#update cooldown
+		if self.shoot_cooldown > 0:
+			self.shoot_cooldown -= 1
+
+
+	def move(self, moving_left, moving_right):
+		#reset movement variables
+		dx = 0
+		dy = 0
+
+		#assign movement variables if moving left or right
+		if moving_left:
+			dx = -self.speed
+			self.flip = True
+			self.direction = -1
+		if moving_right:
+			dx = self.speed
+			self.flip = False
+			self.direction = 1
+
+		#jump
+		if self.jump == True and self.in_air == False:
+			self.gravity = -17
+			self.jump = False
+			self.in_air = True
+
+		#apply FALL
+		self.gravity += FALL
+		if self.gravity > 10:
+			self.gravity
+		dy += self.gravity
+
+		#check for collision
+		for tile in world.obstacle_list:
+			#check collision in the x direction
+			if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+				dx = 0
+			#check for collision in the y direction
+			if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+				#check if below the ground, i.e. jumping
+				if self.gravity < 0:
+					self.gravity = 0
+					dy = tile[1].bottom - self.rect.top
+				#check if above the ground, i.e. falling
+				elif self.gravity >= 0:
+					self.gravity = 0
+					self.in_air = False
+					dy = tile[1].top - self.rect.bottom
+
+
+		#update rectangle position
+		self.rect.x += dx
+		self.rect.y += dy
+
+
+	def shoot(self):
+		if self.shoot_cooldown == 0 and self.ammo > 0:
+			self.shoot_cooldown = 20
+			bullet = Bullet(self.rect.centerx + (0.75 * self.rect.size[0] * self.direction), self.rect.centery, self.direction)
+			bullet_group.add(bullet)
+			#reduce ammo
+			self.ammo -= 1
+
+
+	def ai(self):
+		if self.alive and player.alive:
+			if self.idling == False and random.randint(1, 200) == 1:
+				self.update_action(0)#0: idle
+				self.idling = True
+				self.idling_counter = 50
+			#check if the ai in near the player
+			if self.vision.colliderect(player.rect):
+				#stop running and face the player
+				self.update_action(0)#0: idle
+				#shoot
+				self.shoot()
+			else:
+				if self.idling == False:
+					if self.direction == 1:
+						ai_moving_right = True
+					else:
+						ai_moving_right = False
+					ai_moving_left = not ai_moving_right
+					self.move(ai_moving_left, ai_moving_right)
+					self.update_action(1)#1: run
+					self.move_counter += 1
+					#update ai vision as the enemy moves
+					self.vision.center = (self.rect.centerx + 75 * self.direction, self.rect.centery)
+
+					if self.move_counter > TILE_SIZE:
+						self.direction *= -1
+						self.move_counter *= -1
+				else:
+					self.idling_counter -= 1
+					if self.idling_counter <= 0:
+						self.idling = False
+
+	def update_animation(self):
+		#update animation
+		ANIMATION_COOLDOWN = 100
+		#update image depending on current frame
+		self.image = self.animation_list[self.action][self.frame_index]
+		#check if enough time has passed since the last update
+		if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+			self.update_time = pygame.time.get_ticks()
+			self.frame_index += 1
+		#if the animation has run out the reset back to the start
+		if self.frame_index >= len(self.animation_list[self.action]):
+			if self.action == 3:
+				self.frame_index = len(self.animation_list[self.action]) - 1
+			else:
+				self.frame_index = 0
+
+	def update_action(self, new_action):
+		#check if the new action is different to the previous one
+		if new_action != self.action:
+			self.action = new_action
+			#update the animation settings
+			self.frame_index = 0
+			self.update_time = pygame.time.get_ticks()
+
+	def check_alive(self):
+		if self.health <= 0:
+			self.health = 0
+			self.speed = 0
+			self.alive = False
+			self.update_action(3)
+
+	def draw(self):
+		screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+class World():
+	def __init__(self):
+		self.obstacle_list = []
+
+	def process_data(self, data):
+		#iterate through each value in level data file
+		for y, row in enumerate(data):
+			for x, tile in enumerate(row):
+				if tile >= 0:
+					img = img_list[tile]
+					img_rect = img.get_rect()
+					img_rect.x = x * TILE_SIZE
+					img_rect.y = y * TILE_SIZE
+					tile_data = (img, img_rect)
+					if tile >= 0 and tile <= 17:
+						self.obstacle_list.append(tile_data)
+					elif tile == 23:
+						spike = Spike(img, x * TILE_SIZE, y * TILE_SIZE)
+						spiker_group.add(spike)
+					elif tile >= 18 and tile <= 22:
+						decoration = Decoration(img, x * TILE_SIZE, y * TILE_SIZE)
+						decoration_group.add(decoration)
+					elif tile == 25:#create player
+						player = Hero('player', x * TILE_SIZE, y * TILE_SIZE, 7, 20)
+						health_bar = HealthBar(10, 10, player.health, player.health)
+					elif tile == 26:#create enemies
+						enemy = Hero('enemy', x * TILE_SIZE, y * TILE_SIZE, 2, 20)
+						enemy_group.add(enemy)
+					elif tile == 17:#create ammo box
+						item_box = Item('Ammo', x * TILE_SIZE, y * TILE_SIZE)
+						item_box_group.add(item_box)
+					elif tile == 24:#create health box
+						item_box = Item('Health', x * TILE_SIZE, y * TILE_SIZE)
+						item_box_group.add(item_box)
+					elif tile == 27:#create exit
+						exit = Exit(img, x * TILE_SIZE, y * TILE_SIZE)
+						exit_group.add(exit)
+
+		return player, health_bar
+
+
+	def draw(self):
+		for tile in self.obstacle_list:
+			screen.blit(tile[0], tile[1])
+
+class Decoration(pygame.sprite.Sprite):
+	def __init__(self, img, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
+
+class Exit(pygame.sprite.Sprite):
+	def __init__(self, img, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
+
+
+
+class Item(pygame.sprite.Sprite):
+	def __init__(self, item_type, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.item_type = item_type
+		self.image = item_boxes[self.item_type]
+		self.rect = self.image.get_rect()
+		self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
+
+
+	def update(self):
+		#check if the player has picked up the box
+		if pygame.sprite.collide_rect(self, player):
+			#check what kind of box it was
+			if self.item_type == 'Health':
+				player.health += 25
+				if player.health > player.max_health:
+					player.health = player.max_health
+			elif self.item_type == 'Ammo':
+				pass
+			elif self.item.type == 'Orb':
+				pass
+			#delete the item box
+			self.kill()
+
+
+class HealthBar():
+	def __init__(self, x, y, health, max_health):
+		self.x = x
+		self.y = y
+		self.health = health
+		self.max_health = max_health
+
+	def draw(self, health):
+		#update with new health
+		self.health = health
+		#calculate health ratio
+		ratio = self.health / self.max_health
+		pygame.draw.rect(screen, BLACK, (self.x - 2, self.y - 2, 154, 24))
+		pygame.draw.rect(screen, RED, (self.x, self.y, 150, 20))
+		pygame.draw.rect(screen, GREEN, (self.x, self.y, 150 * ratio, 20))
+
+
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, x, y, direction):
+		pygame.sprite.Sprite.__init__(self)
+		self.speed = 10
+		self.image = bullet_img
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+		self.direction = direction
+
+	def update(self):
+		#move bullet
+		self.rect.x += (self.direction * self.speed)
+		#check if bullet has gone off screen
+		if self.rect.right < 0 or self.rect.left > WIDTH:
+			self.kill()
+		#check for collision with level
+		for tile in world.obstacle_list:
+			if tile[1].colliderect(self.rect):
+				self.kill()
+
+		#check collision with characters
+		if pygame.sprite.spritecollide(player, bullet_group, False):
+			if player.alive:
+				player.health -= 5
+				self.kill()
+		for enemy in enemy_group:
+			if pygame.sprite.spritecollide(enemy, bullet_group, False):
+				if enemy.alive:
+					enemy.health -= 25
+					self.kill()
+
+#create sprite groups
+enemy_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+item_box_group = pygame.sprite.Group()
+decoration_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
+
+
+
+#create empty tile list
+world_data = []
+for row in range(ROWS):
+	r = [-1] * COLUMN
+	world_data.append(r)
+#load in level data and create world
+with open(f'level{level}_data.csv', newline='') as csvfile:
+	reader = csv.reader(csvfile, delimiter=',')
+	for x, row in enumerate(reader):
+		for y, tile in enumerate(row):
+			world_data[x][y] = int(tile)
+world = World()
+player, health_bar = world.process_data(world_data)
+
+run = True
+while run:
+
+	clock.tick(FPS)
+
+	#update background
+	draw_bg()
+	#draw world map
+	world.draw()
+	#show player health
+	health_bar.draw(player.health)
+	#show ammo
+	draw_text('AMMO: ', font, WHITE, 10, 35)
+	for x in range(player.ammo):
+		screen.blit(bullet_img, (90 + (x * 10), 40))
+	
+	player.update()
+	player.draw()
+
+	for enemy in enemy_group:
+		enemy.ai()
+		enemy.update()
+		enemy.draw()
+
+	#update and draw groups
+	bullet_group.update()
+	item_box_group.update()
+	decoration_group.update()
+	exit_group.update()
+	bullet_group.draw(screen)
+	item_box_group.draw(screen)
+	decoration_group.draw(screen)
+	exit_group.draw(screen)
+
+	#update player actions
+	if player.alive:
+		#shoot bullets
+		if shoot:
+			player.shoot()
+		if player.in_air:
+			player.update_action(2)#2: jump
+		elif moving_left or moving_right:
+			player.update_action(1)#1: run
+		else:
+			player.update_action(0)#0: idle
+		player.move(moving_left, moving_right)
+
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+		#keyboard presses
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				moving_left = True
+			if event.key == pygame.K_d:
+				moving_right = True
+			if event.key == pygame.K_SPACE:
+				shoot = True
+			if event.key == pygame.K_w and player.alive:
+				player.jump = True
+			if event.key == pygame.K_ESCAPE:
+				run = False
+
+		#keyboard button released
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_a:
+				moving_left = False
+			if event.key == pygame.K_d:
+				moving_right = False
+			if event.key == pygame.K_SPACE:
+				shoot = False
+
+	pygame.display.update()
+
+pygame.quit()
